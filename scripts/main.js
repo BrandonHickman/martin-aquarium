a0e73ccd1a5bb9a7752d8daf8dc5473a40d0a7e5
Brandon Hickman <brandon_7_77@hotmail.com>
2025-03-17 13:08:20 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+import { database } from './aquariumData.js';
+import { fishList } from './fishList.js'
+//import { tipList } from './tipList.js'
+//import { locationList } from './locationList.js'
+
+// Generate the fish list
+const fishHTML = fishList()
+
+// Generate the care tips
+const tipHTML = tipList()
+
+// Generate the location list
+const locationHTML = locationList()
+
+// Render each HTML string to the correct DOM element
+fishList(fishHTML);
